f6b4ab6524bbc0487321ec7d87a4f40f2b23b824
DM <a>
2015-02-19 19:43:46 +0000

    Corrected strange text

--- src/main/js/CSSSwitcher.js
+++ src/main/js/CSSSwitcher.js
@@ -11,7 +11,6 @@ CSSSwitcher.setSSPath = function (path) { //Sets the path for the theme folder
 }
 CSSSwitcher.loadDefaultStylesheet = function (theme) {
 	if (theme) {
-		break;
 		// Code here
 	}else{
 		console.error("The theme name input has been left blank, please try again with a valid theme name.");
